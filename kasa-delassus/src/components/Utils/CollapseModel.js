110e0f71249f4cb9e8687000f5ff11934dc43b08
rezozero59 <laurentdelassus@gmail.com>
2023-10-12 16:43:13 +0200

    CollapseModel OK, animation à fluidifier.

--- kasa-delassus/src/components/Utils/CollapseModel.js
+++ kasa-delassus/src/components/Utils/CollapseModel.js
@@ -1,20 +1,28 @@
 import React from "react";
 import "./CollapseModel.scss";
+import { useState } from "react";
 
-function CollapseModel() {
+function CollapseModel({ title, txt }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleCollapse = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className="collapse-container">
       <div className="collapse-title">
-        <h3>Titre</h3>
+        <h3>{title}</h3>
         <div className="collapse-icon">
-          <img src="./arrow-up.png" alt="menu collapse" />
+          <img
+            src="./arrow-up.png"
+            alt="menu collapse"
+            onClick={toggleCollapse}
+            style={{ transform: isOpen ? "rotate(180deg)" : "rotate(0deg)" }}
+          />
         </div>
       </div>
-      <div className="collapse-content">
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni sequi
-          odit suscipit quos ad qui maxime eaque accusamus expedita cum!
-        </p>
+      <div className={`collapse-content ${isOpen ? "open" : ""}`}>
+        <p>{txt}</p>
       </div>
     </div>
   );
--- kasa-delassus/src/pages/About.js
+++ kasa-delassus/src/pages/About.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import "./About.scss";
+
 import BannerAbout from "../components/Banner/BannerAbout";
 import CollapseModel from "../components/Utils/CollapseModel";
 
@@ -7,7 +7,22 @@ function About() {
   return (
     <div className="about-container">
       <BannerAbout />
-      <CollapseModel />
+      <CollapseModel
+        title="Fiabilité"
+        txt="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes."
+      />
+      <CollapseModel
+        title="Respect"
+        txt="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+      />
+      <CollapseModel
+        title="Service"
+        txt="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."
+      />
+      <CollapseModel
+        title="Sécurité"
+        txt="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes. "
+      />
     </div>
   );
 }
