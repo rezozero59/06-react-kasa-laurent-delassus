6ac4a99318b27349e265eabe36e9142df994c29a
rezozero59 <laurentdelassus@gmail.com>
2023-10-15 18:05:33 +0200

    Réglage nom/prenom, prochaine étape: rating

--- kasa-delassus/src/pages/Product.js
+++ kasa-delassus/src/pages/Product.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import "./Product.scss";
 
 const Product = ({ appartments }) => {
   const { id } = useParams();
@@ -14,13 +15,41 @@ const Product = ({ appartments }) => {
   if (!appart) {
     return <p>Appartement non trouvé</p>;
   }
+  const [firstName, lastName] = appart.host.name.split(" ");
+
   console.log(appart);
   // (ici le code JSX pour afficher les détails de l'appartement)
 
   return (
-    <div>
-      <h2>{appart.title}</h2>
-      {/* autres éléments HTML/JSX pour afficher les détails de l'appartement */}
+    <div className="product-container">
+      <img src={appart.cover} alt="appartement" className="slide-show" />
+      <div className="information-container">
+        <div className="line-one">
+          <div className="title-location">
+            <h3>{appart.title}</h3>
+            <p>{appart.location}</p>
+          </div>
+          <div className="host">
+            <p>
+              {firstName}
+              <br />
+              {lastName}
+            </p>
+            <img src={appart.host.picture} alt="nom du loueur" />
+          </div>
+        </div>
+        <div className="line-two">
+          <div className="tags-container">
+            {appart.tags.map((tag, index) => (
+              <button key={index} className="tag">
+                {tag}
+              </button>
+            ))}
+          </div>
+          <div className="ratings-container"></div>
+        </div>
+        <div className="line-three"></div>
+      </div>
     </div>
   );
 };
