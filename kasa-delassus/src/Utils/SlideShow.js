e175e9d1fa410f75607b1cc265004c23e33daa67
rezozero59 <laurentdelassus@gmail.com>
2023-10-26 12:09:38 +0200

    Organisation Sass & affichage conditionnel flèches et compteur du carrousel

--- kasa-delassus/src/Utils/SlideShow.js
+++ kasa-delassus/src/Utils/SlideShow.js
@@ -7,6 +7,8 @@ function SlideShow({ images }) {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [isFading, setIsFading] = useState(false);
 
+  const showArrows = images.length > 1;
+
   const handleNext = () => {
     setIsFading(true);
     setTimeout(() => {
@@ -28,25 +30,29 @@ function SlideShow({ images }) {
   return (
     <div className="slide-show-container">
       <div className="slide-show-inner">
-        <button
-          onClick={handlePrev}
-          className="slide-show-arrow slide-show-arrow-left"
-        >
-          <img src={left} alt="précédent" />
-        </button>
+        {showArrows && (
+          <button
+            onClick={handlePrev}
+            className="slide-show-arrow slide-show-arrow-left"
+          >
+            <img src={left} alt="précédent" />
+          </button>
+        )}
         <img
           src={images[currentIndex]}
           alt={`Slide ${currentIndex}`}
           className={`slide-show-image ${isFading ? "fading" : ""}`}
         />
-        <button
-          onClick={handleNext}
-          className="slide-show-arrow slide-show-arrow-right"
-        >
-          <img src={right} alt="suivant" />
-        </button>
+        {showArrows && (
+          <button
+            onClick={handleNext}
+            className="slide-show-arrow slide-show-arrow-right"
+          >
+            <img src={right} alt="suivant" />
+          </button>
+        )}
         <div className="slide-show-index">
-          {currentIndex + 1}/{images.length}
+          {showArrows && currentIndex + 1}/{images.length}
         </div>
       </div>
     </div>
